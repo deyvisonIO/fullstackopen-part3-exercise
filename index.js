cdfb854c31d6a6b13247fbf849d19f8d9334de02
Deyvison Iury <iurydeyvison@gmail.com>
2024-07-30 12:37:23 -0300

    feat: add mongodb integration to website

--- index.js
+++ index.js
@@ -1,158 +1,147 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const Person = require("./models/person");
 
-
 const app = express();
 
-morgan.token("req-body", function(req, res) {
-	if(req.method === 'POST') return JSON.stringify(req.body);
-	return "";
-})
+morgan.token("req-body", function (req, res) {
+  if (req.method === "POST") return JSON.stringify(req.body);
+  return "";
+});
 
-app.use(cors())
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :req-body"))
+app.use(cors());
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :req-body",
+  ),
+);
 app.use(express.json());
-app.use(express.static("dist"))
-
+app.use(express.static("dist"));
 
 app.get("/api/persons", (req, res) => {
-	Person.find({}).then(result => {
-		res.json(result);
-	})
-})
+  Person.find({})
+    .then((result) => {
+      if (result) {
+        res.json(result);
+        return;
+      }
 
+      res.status(404).end();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
 
 app.post("/api/persons", (req, res) => {
-	const { name, number } = req.body;
-	const personToBeAdded = { name, number }
+  const { name, number } = req.body;
+  const personToBeAdded = { name, number };
 
+  if (!name) {
+    res.status(400);
+    res.json({ message: "You need to include a name" });
+    return;
+  }
 
-	if(!id) {
-		res.status(400);
-		res.json({ message: "You need to include a name"})
-		return;
-	}
+  if (!number) {
+    res.status(400);
+    res.json({ message: "You need to include a number" });
+    return;
+  }
 
-	if(!name) {
-		res.status(400);
-		res.json({ message: "You need to include a name"})
-		return;
-	}
-
-	if(!number) {
-		res.status(400);
-		res.json({ message: "You need to include a number"})
-		return;
-	}
-
-	Person.find(personToBeAdded).then(result => {
-		if(result.length === 0) {
-			res.status(400);
-			res.json({ message: "Name must be unique"})
-			return;
-		} else {
-		 Person.create(personToBeAdded).then(result => {
-			res.json(result);	
-			return;
-		})
-		}	
-	})	
-})
-
+  Person.find(personToBeAdded).then((result) => {
+    if (result.length > 0) {
+      res.status(400);
+      res.json({ message: "Name must be unique" });
+      return;
+    } else {
+      Person.create(personToBeAdded).then((result) => {
+        res.json(result);
+        return;
+      });
+    }
+  });
+});
 
 app.put("/api/persons/:id", (req, res) => {
-	const id = req.params.id;
-	const { name, number } = req.body;
+  const id = req.params.id;
+  const { name, number } = req.body;
 
+  if (!id) {
+    res.status(400);
+    res.json({ message: "You need to include an id" });
+    return;
+  }
 
-	if(!id) {
-		res.status(400);
-		res.json({ message: "You need to include an id"})
-		return;
-	}
+  if (!name) {
+    res.status(400);
+    res.json({ message: "You need to include a name" });
+    return;
+  }
 
-	const personExists = people.some(person => person.id === id)
+  if (!number) {
+    res.status(400);
+    res.json({ message: "You need to include a number" });
+    return;
+  }
 
-	if(!personExists) {
-		res.status(400);
-		res.json({ message: "You need to include an id"})
-		return;
-		
-	}
-
-	if(!name) {
-		res.status(400);
-		res.json({ message: "You need to include a name"})
-		return;
-	}
-
-	if(!number) {
-		res.status(400);
-		res.json({ message: "You need to include a number"})
-		return;
-	}
-	
-
-	people = people.map(person => person.id === id ? { id, name, number} : person);
-
-	res.json({ id, name, number });
-})
-
+  Person.findByIdAndUpdate(id, { name, number }).then((result) => {
+    if (!result) {
+      res.status(404);
+      res.json({ message: "Person not found" });
+      return;
+    }
+    res.json(result);
+    return;
+  });
+});
 
 app.get("/api/persons/:id", (req, res) => {
-	const id = req.params.id
+  const id = req.params.id;
 
-	if(!id) {
-		res.status(400).end();
-		return;
-	}
+  if (!id) {
+    res.status(400).end();
+    return;
+  }
 
-	const person = people.find(person => person.id === id);
+  Person.findById(id).then((result) => {
+    if (!result) {
+      res.status(404).end();
+      return;
+    }
 
-	if(!person) {
-		res.status(404).end();
-		return;
-	}
-
-	res.json(person);
-})
+    res.json(result);
+  });
+});
 
 app.delete("/api/persons/:id", (req, res) => {
-	const id = req.params.id
+  const id = req.params.id;
 
-	if(!id) {
-		res.status(400).end();
-		return;
-	}
+  if (!id) {
+    res.status(400).end();
+    return;
+  }
 
-	const person = people.find(person => person.id === id);
-
-	console.log("person:", person);
-	console.log("people:", people);
-
-	if(!person) {
-		res.status(404).end();
-		return;
-	}
-
-	people = people.filter(person => person.id !== id);
-
-
-	res.json(person);
-})
+  Person.findByIdAndDelete(id).then((result) => {
+    if (!result) {
+      res.status(404).end();
+      return;
+    }
+    res.json(result);
+  });
+});
 
 app.get("/info", (req, res) => {
-	const date = new Date();
-	const length = people.length;
-	
+  const date = new Date();
+  const length = people.length;
 
-	res.write(`<p>Phonebook has info for ${length} people</p>`);
-	res.write(`<p>${date}</p>`)
-	res.end()
-})
+  res.write(`<p>Phonebook has info for ${length} people</p>`);
+  res.write(`<p>${date}</p>`);
+  res.end();
+});
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log("Server running!"))
+app.listen(PORT, () => console.log("Server running!"));
